c9a0ab6fc18d6303e2c53c2a116ae92437d4cb4c
Jorjo77 <61197544+Jorjo77@users.noreply.github.com>
2020-11-05 13:33:01 +0200

    Add project files.

---
+++ Encapsulation/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    public class Car
+    {
+        public Engine Engine { get; set; }
+        public SpeedStats SpeedStats { get; set; }
+        public Tires Tires { get; set; }
+
+    }
+}
---
+++ Encapsulation/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    public class Engine 
+    {
+        private Oil oil;//тук като напишем Oil и Ctrl+. ще ни генерира файла (класа)!!!
+        public void Start()
+        {
+            oil.Use()//С Ctrl+. върху Use и ни генерира метода
+        }
+
+        public void Stop()
+        {
+
+        }
+        public void Accelerate()
+        {
+
+        }
+    }
+}
